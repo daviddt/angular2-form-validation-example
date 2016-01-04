f0273a6887feafb3a22291ac8c51e0ef0f652d90
David <daviddentoom@gmail.com>
2016-01-04 22:11:19 +0100

    Update app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -13,27 +13,17 @@ import { UsernameValidator } from './usernameValidator.ts'
 	selector: 'my-app',
 	template: `
 		<form [ngFormModel]="form">
+		  <input type="text" ngControl="username" />
 		
-			<input type="text" ngControl="username" />
-			
-			<p *ngIf="username.pending">Fetching data from the server...</p>
-
-            <div *ngIf="username.dirty && !username.valid && !username.pending">
-				
-                <p *ngIf="username.errors.required">
-                    Username is required.
-                </p>
-				
-				<p *ngIf="username.errors.startsWithNumber">
-                    Your username can't start with a number
-                </p>
-				
-				<p *ngIf="username.errors.usernameTaken">
-                    This username is taken
-                </p>
-            </div>
-        
-			<button (click)="submitData()" [disabled]="!form.valid" class="btn btn-primary">Sumbit data</button>
+		  <p *ngIf="username.pending">Fetching data from the server...</p>
+		
+      <div *ngIf="username.dirty && !username.valid && !username.pending">
+        <p *ngIf="username.errors.required">Username is required.</p>
+			  <p *ngIf="username.errors.startsWithNumber">Your username can't start with a number</p>
+			  <p *ngIf="username.errors.usernameTaken">This username is taken</p>
+      </div>
+      
+		  <button (click)="submitData()" [disabled]="!form.valid" class="btn btn-primary">Sumbit data</button>
 		</form>
 	`,
 	directives: [FORM_DIRECTIVES]
